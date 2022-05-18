42147ee686d92c944d6c34fea09b95382578f7fa
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-05-18 21:53:22 +0100

    Add masthead, add dos/donts

--- src/main/java/io/jenkins/plugins/designlibrary/Symbols.java
+++ src/main/java/io/jenkins/plugins/designlibrary/Symbols.java
@@ -2,14 +2,25 @@ package io.jenkins.plugins.designlibrary;
 
 import hudson.Extension;
 
-/**
- * @author Kohsuke Kawaguchi
- */
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @Extension
 public class Symbols extends UISample {
     @Override
     public String getIconFileName() {
         return "symbol-symbols";
+    }
+
+    public List<String> getSymbols() {
+        List<String> symbols = Arrays.asList("trash", "search", "rss", "ribbon", "reload", "plugins", "jenkins", "folder");
+        Collections.shuffle(symbols);
+        return symbols;
+    }
+
+    public String getSymbol() {
+        return "symbol-" + getSymbols().get(0);
     }
 
     @Extension
