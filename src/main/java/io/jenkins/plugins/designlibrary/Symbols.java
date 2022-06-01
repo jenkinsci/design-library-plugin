257f311ea1dcd04bf1207febfb83a6f1d8ee0891
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-06-01 10:30:23 +0100

    Revamp symbols page (#67)
    * Add masthead, add dos/donts
    
    * WB
    
    * Update content
    
    * Add classes, slow animation
    
    * Remove background from code blocks, override font, add external link icon, i18n
    
    * Update pom.xml
    
    * Fix icons duplication in masthead, override prism colors temp
    
    * Add strings to resource bundle
    
    * Update pom.xml
    
    * Update pom.xml
    
    * build: Bump jenkins.version from incremental to weekly
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>
    Co-authored-by: Alexander Brandes <mc.cache@web.de>

--- src/main/java/io/jenkins/plugins/designlibrary/Symbols.java
+++ src/main/java/io/jenkins/plugins/designlibrary/Symbols.java
@@ -2,18 +2,45 @@ package io.jenkins.plugins.designlibrary;
 
 import hudson.Extension;
 
-/**
- * @author Kohsuke Kawaguchi
- */
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Stack;
+
 @Extension
 public class Symbols extends UISample {
-    @Override
-    public String getIconFileName() {
-        return "symbol-symbols";
-    }
+	@Override
+	public String getIconFileName() {
+		return "symbol-symbols";
+	}
 
-    @Extension
-    public static final class DescriptorImpl extends UISampleDescriptor {
-    }
+	public Stack<String> getSymbols() {
+		Stack<String> symbols = new Stack<>();
+		symbols.addAll(Arrays.asList("trash",
+				"search",
+				"rss",
+				"ribbon",
+				"reload",
+				"plugins",
+				"jenkins",
+				"folder",
+				"play",
+				"fingerprint",
+				"details",
+				"people",
+				"terminal",
+				"edit",
+				"cloud",
+				"analytics",
+				"computer",
+				"download",
+				"hammer",
+				"key"));
+		Collections.shuffle(symbols);
+		return symbols;
+	}
+
+	@Extension
+	public static final class DescriptorImpl extends UISampleDescriptor {
+	}
 }
 
