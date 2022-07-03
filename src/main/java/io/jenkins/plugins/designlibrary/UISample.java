27bbd0089038840c8f5f7031cf8372351acd9601
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-07-03 22:30:09 +0100

    Init

--- src/main/java/io/jenkins/plugins/designlibrary/UISample.java
+++ src/main/java/io/jenkins/plugins/designlibrary/UISample.java
@@ -3,13 +3,28 @@ package io.jenkins.plugins.designlibrary;
 import static org.apache.commons.io.IOUtils.copy;
 
 import hudson.ExtensionPoint;
+import hudson.Functions;
 import hudson.model.Action;
+import hudson.model.Actionable;
 import hudson.model.Describable;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import jenkins.model.Jenkins;
+import jenkins.model.ModelObjectWithContextMenu;
+import org.apache.commons.jelly.JellyContext;
+import org.apache.commons.jelly.JellyException;
+import org.apache.commons.jelly.JellyTagException;
+import org.apache.commons.jelly.Script;
+import org.apache.commons.jelly.XMLOutput;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+import org.kohsuke.stapler.WebApp;
+import org.kohsuke.stapler.jelly.JellyClassTearOff;
+import org.kohsuke.stapler.jelly.JellyFacet;
+import org.xml.sax.helpers.DefaultHandler;
 
 /**
  * @author Kohsuke Kawaguchi
---
+++ src/main/resources/io/jenkins/plugins/designlibrary/search.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const rootUrl = document.querySelector('head').dataset.rooturl
+  const searchBar = document.querySelector("#search-bar")
+  const searchBarResults = document.querySelector("#search-results")
+
+  searchBar.addEventListener("input", () => {
+    const fullUrl = `${rootUrl}/design-library/search?query=${searchBar.value}`;
+
+    searchBarResults.innerHTML = "";
+
+    function appendResults(container, results) {
+      results.forEach(item => {
+        container.append(createElementFromHtml(`<a href="${item.url}">${item.icon} ${item.title}</a>`))
+        const children = createElementFromHtml(`<div></div>`)
+        appendResults(children, item["children"])
+        container.append(children)
+      })
+    }
+
+    fetch(fullUrl)
+      .then(response => response.json())
+      .then(json => {
+
+        appendResults(searchBarResults, json["data"])
+
+        console.log(json)
+      });
+    })
+});
+
+function createElementFromHtml(html) {
+  const template = document.createElement("template");
+  template.innerHTML = html.trim();
+  return template.content.firstElementChild;
+}
