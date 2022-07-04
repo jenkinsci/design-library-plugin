8a5ae29a79f24e8f6553c4020b6782f019f0877c
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-07-04 17:59:21 +0100

    Fix code loading

--- src/main/resources/io/jenkins/plugins/designlibrary/sample.js
+++ src/main/resources/io/jenkins/plugins/designlibrary/sample.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll('.sample-remote')
     .forEach(element => {
       const fileName = element.dataset.sample;
-      const componentName = window.location.href.match(/.+design-library\/(.+)$/)[1]
+      const componentName = window.location.href.match(/.+design-library\/(.[^#]+)?/)[1]
 
       const fullUrl = `${url}/plugin/design-library/${componentName}${fileName}`;
       fetch(fullUrl)
