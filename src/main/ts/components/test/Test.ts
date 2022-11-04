4f9ffc7dc6a820f863067789105f6940193ca121
Thorsten Scherler <scherler@gmail.com>
2022-11-04 16:17:37 +0100

    [sirocco_based] switch to yarn
    Signed-off-by: Thorsten Scherler <scherler@gmail.com>

--- src/main/ts/components/test/Test.ts
+++ src/main/ts/components/test/Test.ts
@@ -2,11 +2,11 @@ import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators'
 import Styles from './Test.styles';
 
-@customElement('swc-test')
+@customElement('jio-test')
 export class Test extends LitElement {
   static styles = [ Styles ];
 
   render() {
-    return html`swc-test`;
+    return html`jio-test`;
   }
 }
