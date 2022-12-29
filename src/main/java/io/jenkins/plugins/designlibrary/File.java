540803f94ac4fc95e3cf4b1a6f69f59ae87eede4
Tim Jacomb <timjacomb1@gmail.com>
2022-12-29 19:59:10 +0000

    Add file upload documentation

---
+++ src/main/java/io/jenkins/plugins/designlibrary/File.java
@@ -0,0 +1,21 @@
+package io.jenkins.plugins.designlibrary;
+
+import hudson.Extension;
+
+@Extension
+public class File extends UISample {
+    @Override
+    public String getDisplayName() {
+        return "File";
+    }
+
+    @Override
+    public String getIconFileName() {
+        return "symbol-cloud-upload-outline plugin-ionicons-api";
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends UISampleDescriptor {
+    }
+}
+
