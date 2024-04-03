56576267b5083653bdb5b234eed68f8b44ee1d56
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-04-03 09:15:29 +0100

    Add banner page (#318)

---
+++ src/main/java/io/jenkins/plugins/designlibrary/Banner.java
@@ -0,0 +1,21 @@
+package io.jenkins.plugins.designlibrary;
+
+import hudson.Extension;
+
+@Extension
+public class Banner extends UISample {
+    @Override
+    public String getDisplayName() {
+        return "Banner";
+    }
+
+    @Override
+    public String getIconFileName() {
+        return "symbol-information-circle-outline plugin-ionicons-api";
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends UISampleDescriptor {
+    }
+}
+
