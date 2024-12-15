5da28b06784717f2ecc6a1348b7f6389bd3cc9f0
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-12-15 20:15:14 +0000

    Rename dropdowns to menu

--- src/main/java/io/jenkins/plugins/designlibrary/Menu.java
+++ src/main/java/io/jenkins/plugins/designlibrary/Menu.java
@@ -3,15 +3,16 @@ package io.jenkins.plugins.designlibrary;
 import hudson.Extension;
 
 @Extension
-public class Dropdowns extends UISample {
+public class Menu extends UISample {
+
     @Override
     public String getIconFileName() {
-        return "symbol-dropdowns";
+        return "symbol-dropdown-menu plugin-design-library";
     }
 
     @Override
     public String getDescription() {
-        return "Dropdowns allow you to group similar controls under one roof. They're an effective way to de-clutter your page whilst offering users the actions they need.";
+        return "Menus allow you to group similar controls under one roof. They're an effective way to de-clutter your page whilst offering users the actions they need.";
     }
 
     @Extension
