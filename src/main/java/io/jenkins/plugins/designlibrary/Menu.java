67c4f8b93b07a0c0179f250ab0ed68528d53ed45
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-01-07 15:49:28 +0000

    Add the version of Jenkins the Menu component was first available

--- src/main/java/io/jenkins/plugins/designlibrary/Menu.java
+++ src/main/java/io/jenkins/plugins/designlibrary/Menu.java
@@ -15,6 +15,11 @@ public class Menu extends UISample {
         return "Menus allow you to group similar controls under one roof. They're an effective way to de-clutter your page whilst offering users the actions they need.";
     }
 
+    @Override
+    public String getSince() {
+        return "2.452.1";
+    }
+
     @Extension
     public static final class DescriptorImpl extends UISampleDescriptor {}
 }
