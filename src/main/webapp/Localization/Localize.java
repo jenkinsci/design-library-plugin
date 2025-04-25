552d32a3b8ac88ea02d40c193dc005cfd4b6b4d6
Markus Winter <m.winter@sap.com>
2025-04-25 21:52:03 +0100

    Add documentation how to make plugins localizable (#408)
    Co-authored-by: Kris Stern <krisstern@outlook.com>
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

---
+++ src/main/webapp/Localization/Localize.java
@@ -0,0 +1,9 @@
+public class Localize {
+    public String getMessage() {
+        return Messages.Localize_message();
+    }
+
+    public String getGreet() {
+        return Messages.Localize_greet("World");
+    }
+}
