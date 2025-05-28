088c71c5e7b0703959830c4df7ec8258f7a64e74
lewisbirks <22620804+lewisbirks@users.noreply.github.com>
2025-05-28 16:56:12 +0100

    linting
    Signed-off-by: lewisbirks <22620804+lewisbirks@users.noreply.github.com>

--- src/main/webapp/Localization/localize-java.js
+++ src/main/webapp/Localization/localize-java.js
@@ -1,9 +1,13 @@
-const i18n = await (await fetch(`${JENKINS_URL}/i18n/resourceBundle?baseName=package.path.to.generated.class`)).json();
+const i18n = await (
+  await fetch(
+    `${JENKINS_URL}/i18n/resourceBundle?baseName=package.path.to.generated.class`,
+  )
+).json();
 
-const localization = i18n['How to localize texts in Jelly'];
+const localization = i18n["How to localize texts in Jelly"];
 const longText = i18n.longtext;
 const hello = i18n.hello;
 
-console.log(localization)
-console.log(longText)
+console.log(localization);
+console.log(longText);
 console.log(hello.replace("{0}", "World!"));
