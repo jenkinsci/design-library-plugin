796fd6ac58473bba4221783963a9604bd925d0e9
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-05-29 12:11:58 +0100

    Fix tabs, tidy up, refine content

--- src/main/webapp/Localization/localize-http.js
+++ src/main/webapp/Localization/localize-http.js
@@ -4,10 +4,8 @@ const response = await fetch(
 const json = await response.json();
 const i18n = json.data;
 
-const localization = i18n["How to localize texts in Jelly"];
 const longText = i18n.longtext;
 const hello = i18n.hello;
 
-console.log(localization);
 console.log(longText);
 console.log(hello.replace("{0}", "World!"));
