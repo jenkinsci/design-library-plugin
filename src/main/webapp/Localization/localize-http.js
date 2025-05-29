d0690a47c98002e5cbf462c76d4dbb33f36a674d
lewisbirks <22620804+lewisbirks@users.noreply.github.com>
2025-05-29 11:51:28 +0100

    linting
    Signed-off-by: lewisbirks <22620804+lewisbirks@users.noreply.github.com>

--- src/main/webapp/Localization/localize-http.js
+++ src/main/webapp/Localization/localize-http.js
@@ -1,6 +1,6 @@
 const response = await fetch(
   `${JENKINS_URL}/i18n/resourceBundle?baseName=io.jenkins.plugins.designlibrary.localize`,
-)
+);
 const json = await response.json();
 const i18n = json.data;
 
