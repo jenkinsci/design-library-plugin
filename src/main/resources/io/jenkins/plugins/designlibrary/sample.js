0629a0b1859b89b291e8f070d7cd1dcac3b10821
Daniel Beck <daniel-beck@users.noreply.github.com>
2025-12-16 00:11:00 +0100

    Make code.jelly compatible with CSP in Jenkins 2.539+

--- src/main/resources/io/jenkins/plugins/designlibrary/sample.js
+++ src/main/resources/io/jenkins/plugins/designlibrary/sample.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const url = document.querySelector("head").dataset.rooturl;
 
+  document.querySelectorAll(".callback-button").forEach((element) => {
+    let callback = element.dataset.callback
+    element.onclick = () => {
+      if (
+        callback &&
+        window[callback] &&
+        typeof window[callback] === "function"
+      ) {
+        window[callback]();
+      } else {
+        console.warn(`Callback function ${callback} is not defined`);
+      }
+    };
+  });
+
   document.querySelectorAll(".sample-remote").forEach((element) => {
     const uiComponentName = element.dataset.componentName;
     const fileName = element.dataset.sample;
