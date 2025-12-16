5346234efcae944c4bfa2438b465524a7176780e
Daniel Beck <1831569+daniel-beck@users.noreply.github.com>
2025-12-16 10:50:16 +0100

    Make `code.jelly` compatible with CSP in Jenkins 2.539+ (#458)
    Co-authored-by: Daniel Beck <daniel-beck@users.noreply.github.com>

---
+++ src/main/resources/io/jenkins/plugins/designlibrary/Notifications/adjunct.js
@@ -0,0 +1,19 @@
+function showDefault() {
+  notificationBar.show("Default");
+}
+
+function showSuccess() {
+  notificationBar.show("Success", notificationBar.SUCCESS);
+}
+
+function showWarning() {
+  notificationBar.show("Warning", notificationBar.WARNING);
+}
+
+function showError() {
+  notificationBar.show("Error", notificationBar.ERROR);
+}
+
+function hideNotification() {
+  notificationBar.hide();
+}
